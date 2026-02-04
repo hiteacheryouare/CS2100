7ec0702b481038d4f32daccfa846e3116c4b09c2
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-02-04 09:21:29 -0500

    Update day_9.py

--- day_9.py
+++ day_9.py
@@ -1,4 +1,6 @@
 import pandas as bamboo
+from pandas.api.types import is_integer_dtype
+import numpy as numbers
 
 ALL_GAMES = "./all_games.csv"
 df = bamboo.read_csv(ALL_GAMES, encoding="utf-8")
@@ -14,3 +16,22 @@ df_copy = df.copy()
 df_copy["total_goals"] = df["home_goal_count"] + df["visiting_goal_count"]
 OT = len(df[df["overtime"] > 0])
 print(f"{OT} games went into OT")
+
+print(df_copy["total_goals"].value_counts())
+print(df_copy.sort_values(by=["date, attendance"], ascending=[True, False], kind="quicksort"))
+
+if is_integer_dtype(df_copy["periods"]):
+    print("yes.")
+else:
+    print("no")
+
+df_copy["game_length"] = df_copy["periods"].map({
+    3: "standard",
+    4: "OT",
+    5: "NOTOT",
+    6: "THREEOT"
+})
+
+coerce = numbers.corrcoef
+
+r = coerce(df_copy["attendance"], df_copy["home_goal_count"]),
